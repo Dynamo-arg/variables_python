9e2b0db246713b4d1e53c9eb98a141ad6155ef9b
Dynamo-arg <72266387+Dynamo-arg@users.noreply.github.com>
2020-10-23 14:25:54 -0300

    Add files via upload

--- ejercicios_profundizacion.py
+++ ejercicios_profundizacion.py
@@ -1,230 +1,189 @@
 #!/usr/bin/env python
 '''
-Tipos de variables [Python]
+Condicionales [Python]
 Ejercicios de profundización
 ---------------------------
 Autor: Inove Coding School
 Version: 1.3
 
 Descripcion:
-Programa creado para que practiquen los conocimietos
-adquiridos durante la semana
+Programa creado para que practiquen los conocimietos adquiridos durante la semana
 '''
 
-__author__ = "Sebas Volpe"
+__author__ = "Inove Coding School"
 __email__ = "alumnos@inove.com.ar"
 __version__ = "1.3"
 
 
 def ej1():
-    # Ejercicios de práctica con números
-    print('Nuestra primera calculadora')
-    '''
-    Realice un calculadora, se ingresará por línea de comando dos
-    números reales y se deberá calcular todas las operaciones entre ellos:
-    - Suma
-    - Resta
-    - Multiplicación
-    - División
-    - Exponente/Potencia
+    print('Ejercicios de práctica con números')
 
-    - Para todos los casos se debe imprimir en pantalla el resultado aclarando
-      la operación realizada en cada caso y con que números
-      se ha realizado la operación
-      ej: La suma entre 4.2 y 6.5 es 10.7
     '''
-    print('Ingrese el primer numero:')
-    numero_1 = int(input())
-    print('Ingrese el segundo numero:')
-    numero_2 = int(input())
-    suma = numero_1 + numero_2
-    resta = numero_1 - numero_2
-    multiplicacion = numero_1 * numero_2
-    division = numero_1 / numero_2
-    exponente = numero_1 ** numero_2
-    print("La suma de", numero_1 ,"y", numero_2 ,"es",suma)
-    print("La resta de", numero_1 ,"y", numero_2 ,"es",resta)
-    print("La multiplicacin de", numero_1 ,"y", numero_2 ,"es",multiplicacion)
-    print("La division de", numero_1 ,"y", numero_2 ,"es",division)
-    print("El exponente de", numero_1 ,"y", numero_2 ,"es",exponente)
+    Realice un programa que solicite por consola 2 números
+    Calcule la diferencia entre ellos e informe por pantalla
+    si el resultado es positivo, negativo o cero.
+    '''
+    numero_1 = int(input('Ingrese el primer número:\n'))
+
+    numero_2 = int(input('Ingrese el segundo número:\n'))
+
+    diferencia = numero_1 - numero_2
+
+    if diferencia > 0:
+        print("El resultado es Positivo")
+    elif diferencia < 0:
+        print("El resultado es Negativo")
+    else:
+        print("El resultado es Cero")
 
 
 def ej2():
-    print('Ejercicios de práctica numérica y cadenas')
-    '''
-    Realice un programa que consulte por consola:
-    - El nombre completo de la persona
-    - El DNI de la persona
-    - La edad de la persona
-    - La altura de la persona
-
-    Finalmente el programa debe imprimir dos líneas de texto por separado
-    - En una línea imprimir el nombre completo y el DNI, aclarando de que
-      campo se trata cada uno
-            Ej: Nombre Completo: Nombre Apellido , DNI:35205070,
-    - En la segunda línea se debe imprimir el nombre completo, edad y
-      altura de la persona
-      Nuevamente debe aclarar el campo de cada uno, para el que lo lea
-      entienda de que se está hablando.
+    print('Ejercicios de práctica con números')
 
     '''
-    print('Ingrese su nombre Completo:')
-    nombre = str(input())
-    
-    print('Ingrese si DNI:')
-    dni = int(input())
+    Realice un programa que solicite el ingreso de tres números
+    enteros, y luego en cada caso informe si el número es par
+    o impar.
+    Para cada caso imprimir el resultado en pantalla.
+    '''
+    numero_1 = int(input('Ingrese el primer número:\n'))
+    if (numero_1 % 2) == 0:
+        print("El", numero_1, "Es par")
+    else:
+        print("El", numero_1, "Es impar")
 
-    print('Ingrese si edad:')
-    edad = int(input())
+    numero_2 = int(input('Ingrese el segundo número:\n'))
+    if (numero_2 % 2) == 0:
+        print("El", numero_2, "Es par")
+    else:
+        print("El", numero_2, "Es impar")
 
-    print('Ingrese su altura en metros:')
-    altura = float(input())
-    
-    print ("Nombre Completo:" ,nombre , "DNI:", dni)
-    print ("Nombre Completo:" ,nombre , "Edad:", edad , "Altura:", altura)
+    numero_3 = int(input('Ingrese el tercer número:\n'))
+    if (numero_3 % 2) == 0:
+        print("El", numero_3, "Es par")
+    else:
+        print("El", numero_3, "Es impar")
 
 
 def ej3():
-    print('Ejercicios de práctica con cadenas')
+    print('Ejercicios de práctica con números')
 
     '''
-    Realice un programa que determine cual sería el apellido de una persona
-    si se ingresaran los dos nombres completos de sus padres.
-    Dicha persona deberá tener los apellidos de ambos padres
+    Realice una calculadora, se ingresará por línea de comando dos números
+    Luego se ingresará como tercera entrada al programa el símbolo de la operación
+    que se desea ejecutar
+    - Suma (+)
+    - Resta (-)
+    - Multiplicación (*)
+    - División (/)
+    - Exponente/Potencia (**)
 
-    - Primero el programa debe consultar el nombre completo del padre_1
-    - Luego el programa debe consultar el nombre completo del padre_2
-    - Luego debe consultar el nombre del hijo/a
-    - Debe extraer los apellidos de los padres
-    - Luego formar el nombre completo del hijo/a utilizando los apellidos
-      de sus padres
-      y el nombre ingresado de dicha persona
-    - Imprimir en pantalla el resultado
-
-    NOTA: Para extraer el apellido del nombre completo recomendamos usar
-    el método "split"
-    Mostraremos un ejemplo:
-
-    direccion_completa = 'Monroe 2716'
-    calle, altura = direccion_completa.split(' ')
-
-    Les dejo por su cuenta a que busquen un poco más acerca de este método
-    que seguramente utilizarán mucho de acá en adelante.
-    Les dejamos un link con algunos ejemplos más:
-    https://www.pythonforbeginners.com/dictionary/python-split
-
-    Cualquier duda con el método split pueden consultarla por el campus
-
+    Se debe efectuar el cálculo correcto según la operación ingresada por consola
+    Imprimir en pantalla la operación realizada y el resultado
     '''
-    print("Ingrese nombre Completo del Padre:")
-    nombre_padre = str(input())
+    numero_1 = int(input('Ingrese el primer número:\n'))
+    numero_2 = int(input('Ingrese el Segundo número:\n'))
 
-    print("Ingrese nombre Completo de la Madre:")
-    nombre_madre = str(input())
+    print("Que operacion desea calcular: + , - , * , / o **:")
+    calcular = str(input())
 
-    print("Ingrese nombre:")
-    nombre = str(input())
-
-    apellido_padre = nombre_padre.split()
-    apellido1 = apellido_padre[-1]
-
-    apellido_madre = nombre_madre.split()
-    apellido2 = apellido_madre[-1]
-
-    print("Su nombre completo es", nombre , apellido1 , apellido2)
-
-
-
+    if calcular == "+":
+        print("La suma es", numero_1 + numero_2)
+    if calcular == "-":
+        print("La resta es", numero_1 - numero_2)
+    if calcular == "*":
+        print("La multiplicaion es", numero_1 * numero_2)
+    if calcular == "/":
+        print("La division es", numero_1 / numero_2)
+    if calcular == "**":
+        print("El exponente es", numero_1 ** numero_2)
 
 
 def ej4():
-    # Ejercicios de práctica con cadenas
-    print('Comencemos a ponernos serios')
+    print('Ejercicios de práctica con cadenas')
+
     '''
-    Realice un programa que determine si una persona_2
-    es pariente de la persona_1
-    Para facilitar el ejercicio solo ingresar un nombre
-    y un apellido por persona
-    Ingresar dichos datos como Nombre Apellido, es decir,
-    primero el nombre y luego el apellido, separado por un espacio.
-    - El programa debe consultar primero el nombre completo de la persona_1
-    - Luego debe consultar el nombre completo de la persona_2
-    - Debe extraer el apellido de la persona_2
-    - Luego preguntar si apellido de la persona_2 está contenido
-      en el nombre completo de la persona_1
-    - En caso de contenerlo, son parientes
-    - Imprimir en pantalla el resultado
+    Realice un programa que solicite por consola 3 palabras cualesquiera
+    Luego el programa debe consultar al usuario como quiere ordenar las palabras
+    1 - Ordenar por orden alfabético (usando el operador ">")
+    2 - Ordenar por cantidad de letras (longitud de la palabra)
 
-    NOTA: Para extraer el apellido del nombre recomendamos
-    usar el método "split"
-    Mostraremos un ejemplo:
+    Si se ingresa "1" por consola se deben ordenar las 3 palabras por orden alfabético
+    e imprimir en pantalla de la mayor a la menor
 
-    direccion_completa = 'Monroe 2716'
-    calle, altura = direccion_completa.split(' ')
-
-    Les dejo por su cuenta a que busquen un poco más acerca
-    de este método que seguramente utilizarán mucho de acá en adelante.
-    Les dejamos un link con algunos ejemplos más:
-    https://www.pythonforbeginners.com/dictionary/python-split
-
-    Cualquier duda con el método split pueden consultarla por el campus
+    Si se ingresa "2" por consola se deben ordenar las 3 palabras por cantidad de letras
+    e imprimir en pantalla de la mayor a la menor
     '''
-    print("Ingrese nombre y apelldido 1:")
-    nombre_1 = str(input())
+    palabra_1 = str(input("Ingrese la primera palabra:\n"))
+    palabra_2 = str(input("Ingrese la primera palabra:\n"))
+    palabra_3 = str(input("Ingrese la primera palabra:\n"))
 
-    print("Ingrese nombre y apellido 2:")
-    nombre_2 = str(input())
+    longitud_1 = len(palabra_1)
+    longitud_2 = len(palabra_2)
+    longitud_3 = len(palabra_3)
+    
+    print("Ingresar 1:(ordenar alfabeticamente) o 2:(Ordenar por cantidad de letras)")
+    orden = int(input())
 
-    apellido_extraer = nombre_2.split()
-    apellido_2 = apellido_extraer[-1]
+    if orden == 1:
+        if (palabra_1 > palabra_2) and (palabra_1 > palabra_3):
+            if palabra_2 > palabra_3:
+                print("Ordenados Alfabeticamente:", palabra_1, palabra_2, palabra_3)
+            else:
+                print("Ordenados Alfabeticamente:", palabra_1, palabra_3, palabra_2)
+        elif (palabra_2 > palabra_1) and (palabra_2 > palabra_3):
+            if palabra_1 > palabra_3:
+                print("Ordenados Alfabeticamente:" ,palabra_2, palabra_1, palabra_3)
+            else:
+                print("Ordenados Alfabeticamente:" ,palabra_2, palabra_3, palabra_1)
+        elif (palabra_3 > palabra_1) and (palabra_3 > palabra_2):
+            if palabra_3 > palabra_2:
+                print("Ordenados Alfabeticamente:" ,palabra_3, palabra_2, palabra_1)
+            else:
+                print(palabra_3, palabra_1, palabra_2)
 
-    contiene = apellido_2 in nombre_1
-
-    print("Son parientes estas dos personas?:" , contiene)
-
+    if orden == 2:
+        if (longitud_1 > longitud_2) and (longitud_1 > longitud_3):
+            if longitud_2 > longitud_3:
+                print("Ordenados Por longitud:", longitud_1, longitud_2, longitud_3)
+            else:
+                print("Ordenados Por longitud:", longitud_1, longitud_3, longitud_2)
+        elif (longitud_2 > longitud_1) and (longitud_2 > longitud_3):
+            if longitud_1 > longitud_3:
+                print("Ordenados Por longitud:" ,longitud_2, longitud_1, longitud_3)
+            else:
+                print("Ordenados Por longitud:" ,longitud_2, longitud_3, longitud_1)
+        elif (longitud_3 > longitud_1) and (longitud_3 > longitud_2):
+            if longitud_3 > longitud_2:
+                print("Ordenados Por longitud:" ,longitud_3, longitud_2, longitud_1)
+            else:
+                print(longitud_3, longitud_1, longitud_2)
 
   
 
+
+
+
+
+
+
 def ej5():
-    # Ejercicios de práctica con cadenas
-    print('Ahora si! buena suerte!')
+    print('Ejercicios de práctica con números')
+
     '''
-    Realice un programa que reciba por consola su nombre completo
-    e imprima en pantalla su nombre en los siguientes formatos:
-    - Todas las letras en minúsculas
-    - Todas las letras en mayúsculas
-    - Solo la primera letra del nombre en mayúscula
+    Realice un programa que solicite ingresar tres valores de temperatura
+    De las temperaturas ingresadas por consola determinar:
+    1 - ¿Cuáles de ellas es la máxima temperatura ingresada?
+    2 - ¿Cuáles de ellas es la mínima temperatura ingresada?
+    3 - ¿Cuál es el promedio de las temperaturas ingresadas?
 
-    NOTA: Para realizar este ejercicio deberá usar los siguientes métodos
-    de strings:
-    - lower
-    - upper
-    - capitalize
-
-    Puede buscar en internet como usar en Python estos métodos.
-    Les dejamos el siguiente link que posee casos de uso de algunos de ellos:
-
-    Link de referencia:
-    https://www.geeksforgeeks.org/isupper-islower-lower-upper-python-applications/
-
-    Cualquier duda con estos métodos pueden consultarla por el campus
+    En cada caso imprimir en pantalla el resultado
     '''
-    print("Ingrese su nombre completo:")
-    Nombre_completo = str(input())
-
-    mayuscula = Nombre_completo.upper()
-    minuscula = Nombre_completo.lower()
-    primera_letra = Nombre_completo.capitalize()
-    
-    print("Su nombre en Mayuscula:",mayuscula)
-    print("Su nombre en Minuscula:",minuscula)
-    print("Solo Mayuscula primer nombre:",primera_letra)    
-
 
 if __name__ == '__main__':
     print("Ejercicios de práctica")
-    ej1()
-    ej2()
-    ej3()
+    #ej1()
+    #ej2()
+    #ej3()
     ej4()
-    ej5()
+    #ej5()
